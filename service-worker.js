bcf0d0d78d6d1f0c8fa89fc37c1237ba2c89f19c
Gianluxx89 <gianluca.difrancisca@gmail.com>
2026-09-14 22:33:39 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,20 @@
-const CACHE_NAME = 'turni-k5-v2';
+const CACHE_NAME = 'turni-k5-v3';
 const ASSETS = [
   '/iBatty/index.html',
   '/iBatty/manifest.json',
-  '/iBatty/icons/icon-192.png',
-  '/iBatty/icons/icon-512.png'
+  '/iBatty/icon-192.png',
+  '/iBatty/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
@@ -24,7 +22,7 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
+    caches.match(event.request).then(cached => {
       if (cached) return cached;
       return fetch(event.request).catch(() => cached);
     })
