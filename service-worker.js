ca870a57e1fed3a50e13b69b07a85ffae653fda4
Gianluxx89 <gianluca.difrancisca@gmail.com>
2026-09-14 21:58:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'turni-k5-v1';
+const CACHE_NAME = 'turni-k5-v2';
 const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  '/iBatty/index.html',
+  '/iBatty/manifest.json',
+  '/iBatty/icons/icon-192.png',
+  '/iBatty/icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -26,12 +26,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
       if (cached) return cached;
-      return fetch(event.request).then((response) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      }).catch(() => cached);
+      return fetch(event.request).catch(() => cached);
     })
   );
 });
